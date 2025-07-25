b7c1f75924326985ab75a5fe7c7569116e305b65
MumphryC <murphycaparas@gmail.com>
2025-07-25 09:30:26 -0700

    Update modern-portfolio.tsx

--- modern-portfolio.tsx
+++ modern-portfolio.tsx
@@ -1307,12 +1307,12 @@ export default function ModernPortfolio() {
 
   {/* Right Side: Logo */}
 
-  <div className="w-32 h-32 bg-white/10 rounded-xl flex items-center justify-center">
+  <div className="w-40 h-40 bg-white/10 rounded-xl flex items-center justify-center">
 
     <img
-      src={edu.logo || "/placeholder.svg?height=100&width=100&query=university logo"}
+      src={edu.logo || "/placeholder.svg?height=160&width=160&query=university logo"}
       alt={edu.school}
-      className="w-24 h-24 object-contain"
+      className="w-32 h-32 object-contain"
 
 
     />
